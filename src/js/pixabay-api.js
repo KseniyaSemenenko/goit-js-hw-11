48efd427d51dee815020129b10315ca6183238a5
Kseniya Semenenko <165210222+KseniyaSemenenko@users.noreply.github.com>
2024-08-18 17:11:04 +0300

    add homework 11

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+const url = 'https://pixabay.com';
+   export const fetchPhotos = inputValue => {
+   const options = new URLSearchParams ({
+    key: "45423395-a55c580a59309a3b931c40bb9",
+    q: inputValue,
+    image_type: "photo",
+    orientation: "horizontal",
+    safesearch: true,
+    
+   }) 
+    return fetch (`${url}/api/?${options}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+}
---
+++ src/main.js
@@ -0,0 +1,60 @@
+// Описаний у документації
+import iziToast from "izitoast";
+// Описаний у документації
+import SimpleLightbox from "simplelightbox";
+import { createGalleryCardTemplate } from './js/render-functions.js'
+import { fetchPhotos } from './js/pixabay-api.js'
+
+const formEl = document.querySelector('.js-form');
+const listEl = document.querySelector('.list-photo');
+
+function formSubmit (event) {
+    event.preventDefault();
+    const inputValue = formEl.elements.img.value.trim();
+    if (inputValue === "") {
+        iziToast.error({
+        message: 'Please enter a search term.',
+    });
+        return
+    }
+    
+  formEl.reset();
+  const loaderEl = document.querySelector('.js-loader');
+  loaderEl.classList.remove('is-hidden');
+  
+  fetchPhotos(inputValue).then(data => {
+  
+    if (data.hits.length === 0) {
+       iziToast.error({
+            message: 'Sorry, there are no images matching your search query. Please try again!',
+       }); 
+        listEl.innerHTML = '';
+        formEl.reset();
+
+        return;
+    }
+    
+     const galleryCardsTemplate = data.hits.map(imgDetails => createGalleryCardTemplate(imgDetails)).join('');
+
+  listEl.innerHTML = galleryCardsTemplate;
+  const newSimple = new SimpleLightbox('.container-img a', { 
+    captions: true,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    
+  });
+  newSimple.refresh();
+})
+    .catch(error => {
+       console.log(error); 
+    })
+    .finally(() => { loaderEl.classList.add('is-hidden'); })
+
+}
+
+formEl.addEventListener('submit', formSubmit);
+
+
+
+
